b6d9fb032b66f7de0659fe66f795a6d06b3e81db
wspropst <wpropst@chapman.edu>
2022-03-27 20:40:17 -0700

    Initial Commit

---
+++ Project2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project2
+//
+//  Created by Liam Propst on 3/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project2/Project2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project2App.swift
+//  Project2
+//
+//  Created by Liam Propst on 3/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
